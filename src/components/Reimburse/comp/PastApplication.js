3b0b9f39db73efb41ac6ed6579b63780f8b0b20e
Kr_Himanshu <155032817+KrHimanshu18@users.noreply.github.com>
2024-12-14 09:06:45 +0530

    Past Application rendered
    Past application can now take props to render information of previous applications

--- src/components/Reimburse/comp/PastApplication.js
+++ src/components/Reimburse/comp/PastApplication.js
@@ -1,31 +1,62 @@
 import React from "react";
+import { useState } from "react";
+import Application from "./Applicatoin";
 
 function PastApplication() {
+  const [application, setApplication] = useState([
+    {
+      prescriptionId: "RX001",
+      billNumber: "B1234",
+      date: "2024-01-01",
+      med: {
+        name: "Paracetamol",
+        quantity: 2,
+        amount: 50,
+      },
+      totalAmount: 100,
+    },
+    {
+      prescriptionId: "RX002",
+      billNumber: "B5678",
+      date: "2024-02-15",
+      med: {
+        name: "Ibuprofen",
+        quantity: 3,
+        amount: 75,
+      },
+      totalAmount: 225,
+    },
+    {
+      prescriptionId: "RX003",
+      billNumber: "B9101",
+      date: "2024-03-10",
+      med: {
+        name: "Amoxicillin",
+        quantity: 1,
+        amount: 200,
+      },
+      totalAmount: 200,
+    },
+  ]);
+
   return (
     <div className="pastApplication">
-      <h1>Past Application</h1>
-      <ol>
-        {/* form a card which will seperately show all the previous application */}
-        <li>
-          <ul>
-            <li>Bill Details</li>
-            <ul>
-              <li>Date</li>
-              <li>Bill No.</li>
-              <ol>
-                {/* form a card for this information */}
-                <li>
-                  <ul>
-                    <li>Medicine 1</li>
-                    <li>Quantity</li>
-                    <li>Amount</li>
-                  </ul>
-                </li>
-              </ol>
-            </ul>
-          </ul>
-        </li>
-      </ol>
+      <h1 className="h3 mb-3 fw-normal ms-4 mt-4">Past Applicatons</h1>
+      {application.map((application, index) => {
+        return (
+          <Application
+            key={index}
+            id={index}
+            prescriptionId={application.prescriptionId}
+            billNumber={application.billNumber}
+            date={application.date}
+            medName={application.med.name}
+            quantity={application.med.quantity}
+            amount={application.med.amount}
+            totalAmount={application.totalAmount}
+          />
+        );
+      })}
     </div>
   );
 }
